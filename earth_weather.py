f5a22751000e2a437f0e8dd5a93a919e80f4c7d3
Kevin Aguero <kevin.nmn.aguero@gmail.com>
2023-12-24 21:01:08 -0700

    Updates to files
    Updating calls and documentation

--- earth_weather.py
+++ earth_weather.py
@@ -4,6 +4,7 @@
 Get the weather data for the location chosen by the user. This leverages the
 Google cloud API to get the GerJSON. Limit its use to avoid having to pay for
 things.
+The Weather API is from the National Weather Service (NWS) api.weather.gov
 
 Google Map API is needed to convert location to a geojson.
 """
@@ -13,12 +14,46 @@ import json
 import googlemaps
 from datetime import datetime
 import globalDecorators as gd
-
-user_agent = 'dars-weather.com kevin.phate@gmail.com'
-base_weather_url = 'https://api.weather.gov'
-GOOGLE_API_KEY = 'AIzaSyAn64xOw5SAj6JTfiE1ZaNHsFzrpYaQKug'
+from main import *
 
 
+def get_location(api_key: str, input_location: str) -> dict:
+    """
+    Get the location in Longitude and Latitude based on the user input. This uses
+    the Google Maps API to convert location to a lat/long.
+
+    Args:
+        api_key (str): The Google Maps API Key
+        input_location (str): User's Input Location. This will be a concat of
+            City, State, Country.
+
+    Returns:
+        geo_location (dict): Keys - lat, lng
+    """
+
+    # Connect to the Google API
+    gmaps = googlemaps.Client(key=api_key)
+
+    # Geocoding an address
+    geocode_result = gmaps.geocode(input_location)
+
+    return geocode_result[0]['geometry']['location']
+
+
+def nsw_url(base_url: str, api_header: str, geo_location: dict) -> str:
+    """
+    Generate a URL to the National Weather Service (NSW) API. The API does not
+    accept more than four decimal places for the lat lng when connecting to the
+    API so always floor the result.
+
+    Args:
+        base_url (str): The base URL of the NSW API
+        api_header (str): The header required to call data from the NSW API
+        geo_location (dict): Keys - lat, lng
+
+    Returns:
+        str: Built URL
+    """
 
 
 def main():
--- main.py
+++ main.py
@@ -10,6 +10,11 @@ import tkinter as tk
 import globalDecorators as gd
 
 
+# Variables
+user_agent_header = 'dars-weather.com kevin.phate@gmail.com'
+base_weather_url = 'https://api.weather.gov'
+google_api_key = 'AIzaSyAn64xOw5SAj6JTfiE1ZaNHsFzrpYaQKug'
+
 def main():
     # Run the main part of the program. Takes no arguments
     pass
